a031a74e1f109dd8c3fd396704aac18131577ec7
XanderSimon <rednax01@gmail.com>
2023-02-20 19:36:36 -0500

    adding bar chart

--- js/d3.v6.1.1/main.js
+++ js/d3.v6.1.1/main.js
@@ -5,13 +5,7 @@ const MARGINS = {left:50, right:50, top:50, bottom:50};
 const VIS_HEIGHT = FRAME_HEIGHT - (MARGINS.top + MARGINS.bottom);
 const VIS_WIDTH = FRAME_WIDTH - (MARGINS.left + MARGINS.right);
 
-// Scaling constants
-const X_SCALE = d3.scaleLinear()
-    .domain([0, 10])
-    .range([0, VIS_WIDTH]);
-const Y_SCALE = d3.scaleLinear()
-    .domain([10, 0])
-    .range([0, VIS_HEIGHT]);
+
 
 const FRAME1 = d3.select("#vis1")
     .append("svg")
@@ -21,6 +15,15 @@ const FRAME1 = d3.select("#vis1")
 
 // read scatter plot data
 d3.csv("data/scatter-data.csv").then((DATA) => {
+
+    // Scaling constants
+    const X_SCALE = d3.scaleLinear()
+        .domain([0, 10])
+        .range([0, VIS_WIDTH]);
+    const Y_SCALE = d3.scaleLinear()
+        .domain([10, 0])
+        .range([0, VIS_HEIGHT]);
+
 	// Plots each of the data points
 	FRAME1.selectAll("points")
         .data(DATA)
@@ -73,3 +76,75 @@ d3.csv("data/scatter-data.csv").then((DATA) => {
     d3.selectAll("#submit").on("click", newPoint);
     d3.selectAll(".point").on("click", clickedPoint);
 });
+
+const FRAME2 = d3.select("#vis2")
+    .append("svg")
+    .attr("height", FRAME_HEIGHT)
+    .attr("width", FRAME_WIDTH)
+    .attr("class", "frame"); 
+
+// read bar chart data
+d3.csv("data/bar-data.csv").then((DATA) => {
+    // find the max Y for the scaling
+    const MAX_Y2 = d3.max(DATA, (d) => { return d.amount; });
+    console.log(MAX_Y2)
+
+    // Scaling constants
+	const X_SCALE = d3.scaleBand()
+        .domain(DATA.map(function(d) { return d.category; }))
+        .range([0, VIS_WIDTH]);
+	const Y_SCALE = d3.scaleLinear()
+        .domain([(MAX_Y2 + 10) ,0])
+        .range([0, VIS_HEIGHT]);
+
+    // Plots each of the bars
+    FRAME2.selectAll("bars")  
+        .data(DATA)
+        .enter()       
+        .append("rect")  
+            .attr("x", (d) => { return X_SCALE(d.category) + MARGINS.left; }) 
+            .attr("y", (d) => { return Y_SCALE(d.amount) +  MARGINS.top; })
+            .attr("width", X_SCALE.bandwidth())
+            .attr("height", (d) => { return VIS_HEIGHT - Y_SCALE(d.amount); })
+            .attr("class", "bar") 
+
+	// Add both axis' to vis2
+	FRAME2.append("g")
+		.attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
+		.call(d3.axisBottom(X_SCALE).ticks(7))
+		.attr("font-size", '15px');  
+	FRAME2.append("g")
+		.attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
+		.call(d3.axisLeft(Y_SCALE).ticks(10))
+		.attr("font-size", '15px');
+
+
+	// Tooltip for bar plot
+    const TOOLTIP = d3.select("#vis2")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    // Show tool tip when mouse hovers over a bar
+    function showToolTip(){
+        TOOLTIP.style("opacity", 1);
+    }
+
+    // Hide tool tip when mouse leaves the area of a bar
+    function hideToolTip(){
+        TOOLTIP.style("opacity", 0);
+    }
+
+    // Moves the tool tip based on where the mouse is, and shows the values of the bar
+    function moveToolTip(event, d){
+        TOOLTIP.html("Category " + d.category + "<br>Value: " + d.amount)
+            .style("left", (event.pageX + 10) + "px") //offset
+            .style("top", (event.pageY - 50) + "px");
+    }
+
+    // Add event listeners
+    d3.selectAll(".bar")
+        .on("mouseover", showToolTip)
+        .on("mouseleave", hideToolTip)
+        .on("mousemove", moveToolTip);
+});  
